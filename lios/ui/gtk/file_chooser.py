ce4500ea966ce5f820f361f0171f1eb39adf3c20
NIhal Rodge <91406248+MrSpideyNihal@users.noreply.github.com>
2025-08-17 08:32:33 +0530

    added explicit Cancel button to FileChooserDialog
    - Updated file_chooser.py to include a Cancel button alongside Open/Save
    - Defined FileChooserDialog.CANCEL constant for consistency
    - Ensures users can explicitly cancel file operations instead of relying only on closing the dialog
    - Maintains compatibility with existing response handling in main.py

--- lios/ui/gtk/file_chooser.py
+++ lios/ui/gtk/file_chooser.py
@@ -18,32 +18,37 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
+
 import gi
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
-
-
 class FileChooserDialog(Gtk.FileChooserDialog):
 	OPEN = Gtk.FileChooserAction.OPEN
 	SAVE = Gtk.FileChooserAction.SAVE
 	OPEN_FOLDER = Gtk.FileChooserAction.SELECT_FOLDER
 	ACCEPT = Gtk.ResponseType.OK
-	
-	def __init__(self,title,action,filters=None,dir=None):
-		if(action == Gtk.FileChooserAction.OPEN or
-			action == Gtk.FileChooserAction.SELECT_FOLDER):
-			super(FileChooserDialog,self).__init__(title,None,action,buttons=(Gtk.STOCK_OPEN,Gtk.ResponseType.OK))
-		else:
-			super(FileChooserDialog,self).__init__(title,None,action,buttons=(Gtk.STOCK_SAVE,Gtk.ResponseType.OK))
-		
-		if (action != Gtk.FileChooserAction.SELECT_FOLDER):
-			filter = Gtk.FileFilter()
-			for item in filters:
-				filter.add_pattern("*."+item)
-			self.add_filter(filter)
+	CANCEL = Gtk.ResponseType.CANCEL   # Added constant
 
-		if (dir):
+	def __init__(self, title, action, filters=None, dir=None):
+		if action in (Gtk.FileChooserAction.OPEN, Gtk.FileChooserAction.SELECT_FOLDER):
+			super(FileChooserDialog, self).__init__(
+				title, None, action,
+				buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+				         Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+		else:
+			super(FileChooserDialog, self).__init__(
+				title, None, action,
+				buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+				         Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+		
+		if action != Gtk.FileChooserAction.SELECT_FOLDER and filters:
+			file_filter = Gtk.FileFilter()
+			for item in filters:
+				file_filter.add_pattern("*." + item)
+			self.add_filter(file_filter)
+
+		if dir:
 			self.set_current_folder(dir)
 
 	#def run() distroy() get_filename()	set_current_folder()
